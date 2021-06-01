346d2def754b48a5c976d546ec6a7ae2622f94e3
Emilio Bravo <78387018+Emilio-Bravo@users.noreply.github.com>
2021-05-31 19:32:20 -0500

    Delete ORM directory

--- ORM/env.php
+++
@@ -1,9 +0,0 @@
-<?php
-$_ENV['DATABASE_DRIVER'] = 'mysql';
-$_ENV['DATABASE_HOST'] = 'localhost';
-$_ENV['DATABASE_NAME'] = 'application';
-$_ENV['DATABASE_USER'] = 'root';
-$_ENV['DATABASE_PASSWORD'] = '';
-$_ENV['DATABASE_PORT'] = '';
-$_ENV['SQLITE_PATH'] = '';
-$_ENV['DATABASE_ERROR_HANDLING'] = 'slient';
