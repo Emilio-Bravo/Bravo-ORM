1a5395b298cc48e0dc72eea6c7b8e1e3af2c2576
Emilio Bravo <78387018+Emilio-Bravo@users.noreply.github.com>
2021-08-05 17:23:46 -0500

    Delete src directory

--- src/interfaces/logicQuerysInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Bravo\ORM;
-
-interface logicQuerys
-{
-    public function where(array $columns_values, string $operator = "=");
-    public function find(array $column_value, string $operator = '=');
-}
